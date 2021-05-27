90ed00afe91db25290baea3592b806d4fd566121
djclayton21 <clayton.daniel.j@gmail.com>
2021-05-27 15:20:38 -0600

    setup mongoDB

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
     const page = renderPage((App) => (props) =>
+      // eslint-disable-next-line react/jsx-props-no-spreading
       sheet.collectStyles(<App {...props} />)
     );
     const styleTags = sheet.getStyleElement();
