661d6bc8ec360c818a9e3985e79561bdd1bca26d
djclayton21 <clayton.daniel.j@gmail.com>
2021-06-15 14:12:43 -0600

    updating cache for pagination

---
+++ sick-fits/frontend/lib/paginationField.js
@@ -0,0 +1,41 @@
+import { PAGINATION_QUERY } from '../components/Pagination';
+
+export default function paginationField() {
+  return {
+    keyArgs: false,
+    read(existing = [], { args, cache }) {
+      const { skip, first } = args;
+
+      // number of items on page from cache
+      const data = cache.readQuery({ query: PAGINATION_QUERY });
+      const count = data?._allProductsMeta?.count;
+      const page = skip / first + 1;
+      const pages = Math.ceil(count / first);
+
+      // check for existing items
+      const items = existing.slice(skip, skip + first).filter((item) => item);
+      if (items.length && items.length !== first && page === pages) {
+        return items;
+      }
+      if (items.length !== first) {
+        return false;
+      }
+
+      if (items.length) {
+        console.log(`there are ${items.length} in the cache. `);
+        return items;
+      }
+
+      return false;
+    },
+    merge(existing, incoming, { args }) {
+      const { skip, first } = args;
+      console.log('merging items from network');
+      const merged = existing ? existing.slice(0) : [];
+      for (let i = skip; i < skip + incoming.length; i += 1) {
+        merged[i] = incoming[i - skip];
+      }
+      return merged;
+    },
+  };
+}
